738359d5e7711af02efcbfd2a3966d25203fbcd9
Lipsa <lipsadas0710@gmail.com>
2025-07-09 18:12:01 +0530

    Missing number

--- Arrays/MissingNum.cpp
+++ Arrays/MissingNum.cpp
@@ -1,8 +1,39 @@
+/*Problem: Find the Missing Number in an Array
+Description:
+Given an array of n-1 integers in the range of 1 to n, find the missing number in the array. The array contains distinct integers.*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm> // for std::reverse
 using namespace std;
 
+/*Brute Force Solution
+Description:
+Take 2 for loops on to count the num and one to iterate the array.*/
+class Solution1 {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        for(int i=0; i<=n;i++){
+            int flag=0;
+            for(int j=0; j<n; j++){
+                if(nums[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+/*Optimal solution(XOR Approach)
+Time complexity:o(n)
+Space complexity:o(1)*/
 class Solution {
   public:
     int missingNum(vector<int>& arr) {
@@ -17,6 +48,23 @@ class Solution {
         }
         XOR1=XOR1^(n+1);
         return XOR1^XOR2;
+    }
+};
+
+/*Optimal Solution (Sum method)
+Description: take the sum of natual no till taht number then sub tact the sum of teh array.
+*/
+class Solution2 {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        int sumNums=0;
+        int sum=n*(n+1)/2;
+        for (int i=0; i<n; i++){
+            sumNums+=nums[i];
+        }
+        int Missing_number=sum-sumNums;
+        return Missing_number;
     }
 };
 
