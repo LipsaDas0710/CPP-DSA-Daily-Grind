15a2c65f2e52443956c4522941221f9d1fa6e17d
Lipsa <lipsadas0710@gmail.com>
2025-07-11 23:11:07 +0530

    Reverse words in string

---
+++ Strings/ReverseWords.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans="";
+        reverse(s.begin(),s.end());
+        for(int i=0; i<s.length();i++){
+            string words="";
+            while(i<s.length() && s[i]!=' '){
+                words += s[i];
+                i++;
+            }
+            reverse(words.begin(),words.end());
+            if(words.length()>0){
+                ans += " " + words;
+            }
+        }
+        return ans.substr(1);
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "the sky is blue"; // Example input
+    string result = sol.reverseWords(s);
+    
+    cout << "Reversed words: " << result << endl; // Output: "blue is sky the"
+    
+    return 0;
+}   
