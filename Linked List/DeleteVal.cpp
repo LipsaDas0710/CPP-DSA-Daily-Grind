939afe61865f56ca43444afd5490132f7f8aafea
Lipsa <lipsadas0710@gmail.com>
2025-07-12 17:20:20 +0530

    Delete ele  of the LL

---
+++ Linked List/DeleteVal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+};
+
+Node* create(vector<int> &arr){
+    Node* head=new Node(arr[0],nullptr);
+    Node* mover=head; 
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i],nullptr);
+        mover->next = temp;
+        mover=temp;
+    }
+    return head; 
+}
+Node* DeleteVal(Node* head , int val){
+   if(head==NULL) return nullptr;
+   Node*temp=head;
+   if(head->data==val){
+        head=head->next;
+        delete temp;
+        return head;
+   }
+
+   Node* prev=nullptr;
+   while(temp!=NULL){
+    if(temp->data==val){
+        // conntect to next next and delete the current
+        prev->next= prev->next->next;
+        delete temp;
+        return head;
+    }
+    prev=temp;
+    temp=temp->next;
+   }
+   return head;
+}
+
+int main(){
+    vector<int> v={1,2,3,4,5};
+    Node* head= create(v);
+    cout << "Linked List before deleting head: ";
+    Node* temp=head;
+    while(temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl;
+    head = DeleteVal(head, 5); // Delete the head node
+    cout << "Linked List after deleting head: ";
+    temp=head;
+    while(temp){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl; // Output the linked list after deleting the head node.
+    
+}
