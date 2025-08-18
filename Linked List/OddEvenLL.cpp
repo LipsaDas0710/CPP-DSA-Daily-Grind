8586f69515c4f8419be7ef729e6fef697da6c677
Lipsa <lipsadas0710@gmail.com>
2025-08-18 10:18:54 +0530

    Odd Even LL

---
+++ Linked List/OddEvenLL.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+void oddEvenList(Node* head){
+    Node* odd=head;
+    Node* even=head->next;
+    Node* evenHead=even;
+
+    while (even!=NULL && even->next!=NULL){
+        odd->next=odd->next->next;
+        even->next=even->next->next;
+
+        odd=odd->next;
+        even=even->next;
+
+    }
+    odd->next=evenHead;
+  
+}
+
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    oddEvenList(head);
+
+    cout << "Modified List: ";
+    printList(head);
+
+    // Free allocated memory
+    Node* current = head;
+    while (current) {
+        Node* nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+
+    return 0;
+}
