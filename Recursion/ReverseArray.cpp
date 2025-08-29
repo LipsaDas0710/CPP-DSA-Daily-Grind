1c280b72ea62b69aac13e4dc2826a8bb811fc8b6
Lipsa <lipsadas0710@gmail.com>
2025-08-29 14:25:27 +0530

    Reverse an array using recursion

---
+++ Recursion/ReverseArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void Reverse(int arr[],int l, int r){
+    if(l<=r) return;
+    swap(arr[l],arr[r]);
+    return Reverse(arr,l+1,r-1);
+}
+
+int main(){
+    int arr[5]={10,20,30,40,50};
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    Reverse(arr,0,4);
+    cout<< "After Reversing "<<endl;
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
