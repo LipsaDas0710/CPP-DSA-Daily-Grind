19251e6e5f144f1c58077534a130e444bebf2ea5
Lipsa <lipsadas0710@gmail.com>
2025-08-30 12:27:53 +0530

    Leetcode 3021

--- PROBLEMS/PickNumPlie.cpp
+++ PROBLEMS/PickNumPlie.cpp
@@ -1,10 +1,4 @@
 // 3021. Alice and Bob Playing Flower Game
-// Solved
-// Medium
-// Topics
-// premium lock icon
-// Companies
-// Hint
 // Alice and Bob are playing a turn-based game on a field, with two lanes of flowers between them. There are x flowers in the first lane between Alice and Bob, and y flowers in the second lane between them.
 
 
