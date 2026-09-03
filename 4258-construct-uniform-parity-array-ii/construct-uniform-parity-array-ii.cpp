6ac9aed2cd139fca658d62557c8dc7661c4b1446
Lipsa Das <150002623+LipsaDas0710@users.noreply.github.com>
2026-09-03 23:33:44 +0530

    Time: 4 ms (65.18%) | Memory: 165.8 MB (87.43%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+//Lipsa
+    bool uniformArray(vector<int>& nums1) {
+        int min_val = nums1[0];
+        bool has_odd = false;
+
+        for (int x : nums1) {
+            if (x < min_val) {
+                min_val = x;
+            }
+            if (x % 2 != 0) {
+                has_odd = true;
+            }
+        }
+
+        if (min_val % 2 != 0) {
+            return true;
+        }
+
+        return !has_odd;
+    }
+};
